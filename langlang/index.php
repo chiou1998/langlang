8e1489479ae0ae5d489a83e70443d077144d228c
chiou1998 <2601424573@qq.com>
2020-11-15 19:25:04 +0800

    1

--- langlang/index.php
+++ langlang/index.php
@@ -130,14 +130,14 @@
                 care for teeth to create a better future</p>
         </div>
         <div class="list-info"> 
-<div id="i1">
+    <div id="i1">
         <div class="loading-wait">
                 加载中
                 <div>
                 <svg t="1604829803210" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="972" width="200" height="200"><path d="M512 46.4c0 18.3 15.2 32.8 33.4 32.2 5.9-0.2 11.9-0.3 17.9-0.3 129.2 0 247.3 47.9 337.4 126.9 4.4 3.9 10.5-2.1 6.8-6.6-87.3-105.4-216-175.1-361.1-184.4-18.6-1.1-34.4 13.5-34.4 32.2z m0 931.3c0-18.3-15.2-32.8-33.4-32.2-5.9 0.2-11.9 0.3-17.9 0.3-129.2 0-247.3-47.9-337.4-126.9-4.4-3.9-10.5 2.1-6.8 6.6 87.3 105.2 216 174.9 361.1 184.2 18.6 1.3 34.4-13.4 34.4-32z" p-id="973" fill="#1296db"></path></svg>
             </div>
             </div>
-</div>
+    </div>
             <ul class="clearfix">
                            
         </ul>
@@ -233,18 +233,34 @@
        type: "POST",                 //GET or POST
        beforeSend: function(){},
        success: function(req){
+
       let i2 = document.getElementById('i1');
+
+      if(!req.data || req.data.length === 0){//判断数据是否存在
+          i2.innerHTML = `<div class="loading-wait">
+                加载中111
+                <div>
+                <svg t="1604829803210" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="972" width="200" height="200"><path d="M512 46.4c0 18.3 15.2 32.8 33.4 32.2 5.9-0.2 11.9-0.3 17.9-0.3 129.2 0 247.3 47.9 337.4 126.9 4.4 3.9 10.5-2.1 6.8-6.6-87.3-105.4-216-175.1-361.1-184.4-18.6-1.1-34.4 13.5-34.4 32.2z m0 931.3c0-18.3-15.2-32.8-33.4-32.2-5.9 0.2-11.9 0.3-17.9 0.3-129.2 0-247.3-47.9-337.4-126.9-4.4-3.9-10.5 2.1-6.8 6.6 87.3 105.2 216 174.9 361.1 184.2 18.6 1.3 34.4-13.4 34.4-32z" p-id="973" fill="#1296db"></path></svg>
+            </div>
+            </div>`
+            return false
+      }
       let ul = '<ul class=""clearfix>'
        req.data.forEach(item => {
           ul +=
-          `<figure>
+          `
+          <figure>
+           <a href="https://www.runoob.com/?id=${item.id}" style=" text-decoration: none;  color:black">
                 <div class="scale-p">
                     <img src="${item.imgUrl}">
                 </div>               
                 <figcaption>${item.title}</figcaption>
                 <p>${item.dec}</p>
-            </figure>`         
+                </a>
+            </figure>            
+            `         
       })
+        ul += '</ul>'
         i2.innerHTML = ul
 
        },       //请求成功时的处理
