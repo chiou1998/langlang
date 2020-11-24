f6c5fa4d07d842f697a94d66a3e46869ace745fe
chiou1998 <2601424573@qq.com>
2020-11-24 19:49:41 +0800

    1

--- langlang/index.php
+++ langlang/index.php
@@ -146,6 +146,7 @@
         </div>
             </header>
             <div class="section-one ">
+                
                 <div id="w11">
                     
                 </div>
@@ -289,7 +290,8 @@
 <script src="./js/common.js"></script>
 <script>
     loading({
-        message:'加载中...'
+        id:'i1',
+        message:'加载中上1'
     })
     $.ajax({
        url: "./data/index.php",   //请求的url地址
@@ -306,12 +308,10 @@
       let i2 = document.getElementById('i1');
 
       if(!req.data || req.data.length === 0){//判断数据是否存在
-          i2.innerHTML = `<div class="loading-wait">
-                加载中111
-                <div>
-                <svg t="1604829803210" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="972" width="200" height="200"><path d="M512 46.4c0 18.3 15.2 32.8 33.4 32.2 5.9-0.2 11.9-0.3 17.9-0.3 129.2 0 247.3 47.9 337.4 126.9 4.4 3.9 10.5-2.1 6.8-6.6-87.3-105.4-216-175.1-361.1-184.4-18.6-1.1-34.4 13.5-34.4 32.2z m0 931.3c0-18.3-15.2-32.8-33.4-32.2-5.9 0.2-11.9 0.3-17.9 0.3-129.2 0-247.3-47.9-337.4-126.9-4.4-3.9-10.5 2.1-6.8 6.6 87.3 105.2 216 174.9 361.1 184.2 18.6 1.3 34.4-13.4 34.4-32z" p-id="973" fill="#1296db"></path></svg>
-            </div>
-            </div>`
+        loading({
+        id:'i1',
+        message:'加载中上2'
+    })
             return false
       }
       let ul = '<ul class=""clearfix>'
@@ -337,6 +337,11 @@
        error: function(){}          //请求出错处理
     });
 
+
+    loading({
+        id:'w11',
+        message:'加载中下1'
+    })
     $.ajax({
        url: "./data/index.php",   //请求的url地址
        dataType:"json",           //返回的格式为json
@@ -349,6 +354,18 @@
        type: "POST",                 //GET or POST
        beforeSend: function(){},
        success: function(req){
+
+
+
+
+        if(!req.data || req.data.length === 0){//判断数据是否存在
+            loading({
+         id:'w11',
+        message:'加载中下2'
+    })
+            return false
+      }
+
         let wrap1 = document.getElementById('w11')
         let data = req.data
         let html = ''
